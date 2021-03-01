d0608e2b6f36c94c93a8f8f0db5ef5c209d34a7a
Laurynrp29 <42683856+Laurynrp29@users.noreply.github.com>
2021-02-28 23:09:09 -0500

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Lauryn Pavlik on 2/28/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+  
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
